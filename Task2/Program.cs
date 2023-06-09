fb0147814c4f7f9d00698d64e22a13ac247aed1f
Goldendenday <bieltikov99@mail.ru>
2023-06-09 23:24:46 +0300

    Make Task#2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое трехзначное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе трехзначное число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье трехзначное число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine(number1);
+}
+if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine(number2);
+}
+if (number3 > number1 && number3 > number2)
+{
+    Console.WriteLine(number3);
+}
